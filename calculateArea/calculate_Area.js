7b131c0b97f8d06ecd47d02269647e7a87f032eb
Enes Kafa <eneskafa20@gmail.com>
2024-07-23 07:43:31 -0400

    added a new func

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,5 +1,8 @@
 let length;
 let width;
+let grocery1;
+let grocery2;
+let grocery3;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
@@ -8,4 +11,13 @@ function calculateArea() {
     let area = length * width;
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
+
+function groceryTracker() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let sum = grocery1 + grocery2 + grocery3;
+    document.getElementById('result2').innerText = `The total amount is: $ ${sum}`;
 }
